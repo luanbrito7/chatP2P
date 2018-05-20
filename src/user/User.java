e276c045f05c8a14acbc3af964a84b6f5c60e9df
Luan Antunes de Brito <lab7@WINDOWS.CIN.UFPE.BR>
2018-05-20 13:32:58 -0300

    fiz quase nada

--- src/user/User.java
+++ src/user/User.java
@@ -1,11 +1,15 @@
 package user;
 import java.io.Serializable;
+import client.TCPClient;
+import server.TCPServer;
 
 public class User implements Serializable{
 
 	private String name;
 	private String ipAddress;
 	private int port;
+	public TCPClient cliente;
+	public TCPServer server;
 	
 	public User(String name, String ipAddress, int port){
 		this.name = name;
